77f7e5f3350a29fc752495b79342d8dde79657f7
Samitha <samitha@Samitha-Calcey.local>
2023-12-22 19:56:54 +0530

    fix: test update

--- __tests__/Auth/index.test.ts
+++ __tests__/Auth/index.test.ts
@@ -4,7 +4,7 @@ import { action } from "../helpers";
 describe("Appium with Jest automation testing", () => {
   test("First test", async function () {
     await action(client, "click", "sign-in-button");
-    await action(client, "setValue", "sign-in-email", "samitha+qa@calcey.com");
+    await action(client, "setValue", "sign-in-email", "joe@doe.com");
     await action(client, "setValue", "sign-in-password", "User@123");
     await action(client, "click", "sign-in-next-button");
   });
